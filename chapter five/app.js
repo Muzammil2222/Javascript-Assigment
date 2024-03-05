b915b56270bf4705adfaa32501a26d0a1409831d
Muzammil2222 <142160942+Muzammil2222@users.noreply.github.com>
2024-03-05 13:53:07 +0500

    added files

---
+++ chapter five/app.js
@@ -0,0 +1,61 @@
+
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+
+// var num = 3 ;
+// var num2 = 5;
+// document.write("Sum of " + num + " and " + num2 + " is ");
+// document.write(num + num2);
+
+// var subtraction = 5 + 2;
+// var multiplication = 3 * 2;
+// var division = 6 / 2;
+// var modulus = 2 % 13;
+// alert(subtraction);
+// alert(multiplication);
+// alert(division);
+// alert(modulus);
+// document.write(subtraction , multiplication , division , modulus );
+// var others;
+// document.write("Value after variable declaration is " + others);
+// var numVeriable = 5;
+// document.write("Initial value: " + numVeriable + "<br>");
+// numVeriable++;
+// document.write("Value after increment is: " + numVeriable + "<br>");
+// numVeriable += 7;
+// document.write("Value after addition is: " + numVeriable + "<br>");
+// numVeriable--;
+// document.write("Value after decrement is: " + numVeriable + "<br>");
+// var remainder = numVeriable % 3;
+// document.write("The remainder is: " + remainder);
+// var costOfticket = 600;
+// var totalTicket = 5;
+// document.write("<br>Total cost to buy 5  tickets to a movie is " + costOfticket * totalTicket + "PKR");
+var table = "table 0f 4";
+var fourtable = 4;
+document.write(table);
+document.write("<br><strong>4x1=" + fourtable*1 + "<strong>" );
+document.write("<br><strong>4x2=" + fourtable*2 + "<strong>" );
+document.write("<br><strong>4x3=" + fourtable*3 + "<strong>" );
+document.write("<br><strong>4x4=" + fourtable*4 + "<strong>" );
+document.write("<br><strong>4x5=" + fourtable*5 + "<strong>" );
+document.write("<br><strong>4x6=" + fourtable*6 + "<strong>" );
+document.write("<br><strong>4x7=" + fourtable*7 + "<strong>" );
+document.write("<br><strong>4x8=" + fourtable*8 + "<strong>" );
+document.write("<br><strong>4x9=" + fourtable*9 + "<strong>" );
+document.write("<br><strong>4x10=" + fourtable*10 + "<strong><br>" );
+
+// a. Store a Celsius temperature into a variable.
+var celsiusTemp = 30;
+
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+var fahrenheitTemp = (celsiusTemp * 9 / 5) + 32;
+document.write(celsiusTemp + "°C is " + fahrenheitTemp + "°F<br>");
+
+// c. Now store a Fahrenheit temperature into a variable.
+var fahrenheitTemp2 = 86;
+
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+var celsiusTemp2 = (fahrenheitTemp2 - 32) * 5 / 9;
+document.write(fahrenheitTemp2 + "°F is " + celsiusTemp2 + "°C");
+
+
---
+++ chapter two/app.js
@@ -0,0 +1,23 @@
+// var myName = 'Muzammil';
+// alert(myName);
+// var mynam = "My Name";
+// var nam = " is Muzammil";
+// alert(mynam + nam);
+// var num = 15;
+// var numAge = " Year Old";
+// alert(num + numAge);
+// var cert = "Certified";
+// var developer = " Mobile Application Development";
+// alert(cert + developer)
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(pizza);
+var email = "example@example.com";
+alert("Your email address is: " + email);
+
+
+var showWork = 'A smarter way to learn JavaScript';
+alert("i am trying to learn from the Book " + showWork);
+var docuwrite = "Yah! I can write HTML content through JavaScript";
+document.write(docuwrite);
+var working = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(working);
